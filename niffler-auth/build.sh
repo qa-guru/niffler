2dd701558451db7960b20f350079b2f80c9c8ec1
Dmitrii Tuchs <dtuchs@gmail.com>
2023-02-05 19:14:49 +0600

    Added docker-compose for backend

--- niffler-auth/build.sh
+++
@@ -1,9 +0,0 @@
-source ./docker.properties
-
-echo DEB - ${VERSION}
-
-# Build project
-gradle build
-
-# Build docker image
-docker build --build-arg APP_VER=${VERSION} -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:latest .
