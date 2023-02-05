770b48e919bc06aef9aae7bf4cf95a42603c777b
alx.krw <sashkir7@mail.ru>
2023-02-05 15:27:53 +0600

    Add converter: from json to userDto

---
+++ niffler-e-2-e-kireev-tests/src/test/java/niffler/jupiter/user/JsonToUserConverter.java
@@ -0,0 +1,29 @@
+package niffler.jupiter.user;
+
+import lombok.SneakyThrows;
+import niffler.api.user.dto.UserDto;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ArgumentConverter;
+
+import java.io.InputStream;
+
+final class JsonToUserConverter implements ArgumentConverter {
+
+    private final ClassLoader classLoader = getClass().getClassLoader();
+
+    @Override
+    @SneakyThrows
+    public UserDto convert(Object source, ParameterContext context)
+            throws ArgumentConversionException {
+        if (!(source instanceof String))
+            throw new ArgumentConversionException("Work only for Strings!");
+
+        try (InputStream is = classLoader.getResourceAsStream((String) source)) {
+            assert is != null;
+            return UserDto.fromJson(IOUtils.toString(is));
+        }
+    }
+
+}
