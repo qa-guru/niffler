d1fe54bb0d76ab60ad762eae234ea6aed2a12910
Dmitrii Tuchs <dtuchs@gmail.com>
2023-04-14 13:52:26 +0600

    fix decline invitations

--- niffler-userdata/src/main/java/niffler/data/repository/FriendsRepository.java
+++ niffler-userdata/src/main/java/niffler/data/repository/FriendsRepository.java
@@ -1,12 +1,13 @@
 package niffler.data.repository;
 
 import niffler.data.FriendsEntity;
+import niffler.data.FriendsId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface FriendsRepository extends JpaRepository<FriendsEntity, UUID> {
+public interface FriendsRepository extends JpaRepository<FriendsEntity, FriendsId> {
 
-    List<FriendsEntity> findAllByUserId(UUID userId);
+
 }
