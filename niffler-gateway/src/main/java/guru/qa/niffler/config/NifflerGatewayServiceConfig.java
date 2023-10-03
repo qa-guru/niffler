abaef6f59e391a60d9071b16f8cc50938dfab2dc
dtuchs <dtuchs@gmail.com>
2023-10-03 17:16:57 +0600

    redeploy gateway

--- niffler-gateway/src/main/java/guru/qa/niffler/config/NifflerGatewayServiceConfig.java
+++ niffler-gateway/src/main/java/guru/qa/niffler/config/NifflerGatewayServiceConfig.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class NifflerGatewayServiceConfig {
 
-    public static final int THREE_MB = 3145728;
+    public static final int TEN_MB = 10 * 1024 * 1024;
 
     private final String nifflerUserdataBaseUri;
 
@@ -23,6 +24,9 @@ public class NifflerGatewayServiceConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(TEN_MB))
+                        .build())
                 .build();
     }
 
