126016f8a5119e316a703828c611d0ab7bfb7d5f
dtuchs <dtuchs@gmail.com>
2025-03-30 01:41:35 +0500

    log session id & cookies

--- niffler-auth/src/main/java/guru/qa/niffler/service/GlobalExceptionHandler.java
+++ niffler-auth/src/main/java/guru/qa/niffler/service/GlobalExceptionHandler.java
@@ -1,17 +1,66 @@
 package guru.qa.niffler.service;
 
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+  public static final String ERROR_MESSAGE_PARAM = "errorMessage";
+  public static final String ORIGINAL_CODE_PARAM = "originalCode";
+  public static final String SESSION_ID_PARAM = "sessionId";
+
+  private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
   @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-  @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-  public String handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+  @ResponseStatus(HttpStatus.FOUND)
+  public String handleMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    LOG.error("### Resolve HttpRequestMethodNotSupportedException in @RestControllerAdvice and redirect to /error page", ex);
+    logRequestData(request, redirectAttributes, ex, HttpStatus.METHOD_NOT_ALLOWED);
     return "redirect:/error";
+  }
+
+  @ExceptionHandler(NoResourceFoundException.class)
+  @ResponseStatus(HttpStatus.FOUND)
+  public String handleNoResourceFoundException(NoResourceFoundException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    LOG.error("### Resolve NoResourceFoundException in @RestControllerAdvice and redirect to /error page", ex);
+    logRequestData(request, redirectAttributes, ex, HttpStatus.NOT_FOUND);
+    return "redirect:/error";
+  }
+
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.FOUND)
+  public String handleException(Exception ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
+    LOG.error("### Resolve Exception in @RestControllerAdvice and redirect to /error page", ex);
+    logRequestData(request, redirectAttributes, ex, HttpStatus.INTERNAL_SERVER_ERROR);
+    return "redirect:/error";
+  }
+
+  private void logRequestData(@Nullable HttpServletRequest request,
+                              @Nonnull RedirectAttributes redirectAttributes,
+                              @Nonnull Exception ex,
+                              @Nonnull HttpStatus status) {
+    if (request != null) {
+      Cookie[] cookies = request.getCookies();
+      if (cookies != null) {
+        for (Cookie cookie : cookies) {
+          LOG.info("### Cookie: {} = {}", cookie.getName(), cookie.getValue());
+        }
+      }
+      redirectAttributes.addFlashAttribute(SESSION_ID_PARAM, request.getSession().getId());
+    }
+    redirectAttributes.addFlashAttribute(ERROR_MESSAGE_PARAM, ex.getMessage());
+    redirectAttributes.addFlashAttribute(ORIGINAL_CODE_PARAM, status.value());
   }
 }
