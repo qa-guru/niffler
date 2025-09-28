2327f1ef03fbf007e2f0f2d746bb8a4dbf3e39ae
Irina Styazhkina <istyazhkina@ozon.ru>
2025-09-28 17:37:18 +0300

    fix service worker

--- niffler-ng-client/public/firebase-messaging-sw.js
+++ niffler-ng-client/public/firebase-messaging-sw.js
@@ -22,25 +22,27 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  const { title, body, icon, click_action } = payload.notification ?? {};
-  self.registration.showNotification(title || 'Notification', {
-    body: body || '',
-    icon: icon || '/icon.png',
-    data: { click_action: click_action || '/' }
-  });
+    console.log("fcm")
+    const {title, body, icon, click_action} = payload.notification ?? {};
+    console.log(payload.notification);
+    self.registration.showNotification(title || 'Notification', {
+        body: body || '',
+        icon: icon || '/icon.png',
+        data: {click_action: click_action || '/'}
+    });
 });
 
 self.addEventListener('notificationclick', (event) => {
-  event.notification.close();
-  const url = event.notification?.data?.click_action || '/';
-  event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
-      for (const client of clientList) {
-        if ('focus' in client && client.url.includes(self.origin)) return client.focus();
-      }
-      return clients.openWindow(url);
-    })
-  );
+    event.notification.close();
+    const url = event.notification?.data?.click_action || '/';
+    event.waitUntil(
+        clients.matchAll({type: 'window', includeUncontrolled: true}).then((clientList) => {
+            for (const client of clientList) {
+                if ('focus' in client && client.url.includes(self.origin)) return client.focus();
+            }
+            return clients.openWindow(url);
+        })
+    );
 });
 
 self.addEventListener("install", event => {
@@ -72,3 +74,35 @@ self.addEventListener("fetch", (event) => {
         })());
     }
 });
+
+self.addEventListener("push", function (event) {
+    console.log("push handler");
+    const data = event.data.json();
+    console.log(data);
+    const {title, message, interaction} = data;
+
+    const options = {
+        body: message,
+        icon: '/pwa/launchericon-512x512.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now()
+        },
+        actions: [
+            {
+                action: 'confirm',
+                title: 'OK'
+            },
+            {
+                action: 'close',
+                title: 'Close notification'
+            },
+        ],
+        requireInteraction: interaction
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(title, options)
+    );
+});
+
